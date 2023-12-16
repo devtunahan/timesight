6b92f77b980f8aef2c061a973535c5fb9a08ac60
Tunahan <47463551+devtunahan@users.noreply.github.com>
2023-12-16 19:26:48 +0100

    getting open applications

--- src/main/java/at/tunahan/Main.java
+++ src/main/java/at/tunahan/Main.java
@@ -1,7 +1,40 @@
 package at.tunahan;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  List<String> processes = getLinuxProcesses();
+
+        // Print the process names
+        for (String process : processes) {
+            System.out.println(process);
+        }
     }
+
+    private static List<String> getLinuxProcesses() {
+        List<String> processes = new ArrayList<>();
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("wmctrl", "-l");
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                    processes.add(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                System.err.println("Error executing wmctrl command. Exit code: " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return processes;
+    }        
 }
